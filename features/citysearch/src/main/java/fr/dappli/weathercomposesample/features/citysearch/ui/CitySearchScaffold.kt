03dad7e1caf672878bb606042ef0f3be6f34dcb5
Andrei Mandychev <andrei.mandychev@dappli.fr>
2021-11-14 18:58:59 +0100

    add searchview

--- features/citysearch/src/main/java/fr/dappli/weathercomposesample/features/citysearch/ui/CitySearchScaffold.kt
+++ features/citysearch/src/main/java/fr/dappli/weathercomposesample/features/citysearch/ui/CitySearchScaffold.kt
@@ -1,13 +1,14 @@
 package fr.dappli.weathercomposesample.features.citysearch.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.appcompat.widget.SearchView
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import fr.dappli.weathercomposesample.features.citysearch.R
 
 @Composable
 fun CitySearchScaffold(
@@ -23,6 +24,31 @@ fun CitySearchScaffold(
                     .padding(start = 16.dp, top = 32.dp, end = 16.dp)
             ) {
                 CitySearchInput(city, onCityNameChange)
+                Spacer(modifier = Modifier.height(16.dp))
+                AndroidView(
+                    modifier = Modifier.fillMaxWidth(),
+                    factory = { context ->
+                        // TODO apply dark theme
+                        SearchView(context).apply {
+                            println("andrei redraw SearchView")
+                            setBackgroundResource(R.drawable.search_background)
+                            isIconified = false
+                            clearFocus()
+                            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                                override fun onQueryTextSubmit(query: String?) = true
+
+                                override fun onQueryTextChange(newText: String?): Boolean {
+                                    newText?.let { text -> onCityNameChange(text) }
+                                    return true
+                                }
+                            })
+                        }
+                    },
+                    update = { searchView ->
+                        // TODO update adapter
+                    }
+                )
+
             }
         },
         bottomBar = {
